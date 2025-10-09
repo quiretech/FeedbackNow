68650dfdd57049fc955e18f494d4ed9dcdb6acac
jpandya <jpandya@umass.edu>
2025-10-08 20:59:09 -0400

    clean up

--- zsk/lvgl_1/src/image_bitmap.h
+++
@@ -1,9 +0,0 @@
-#ifndef IMAGE_BITMAP_H
-#define IMAGE_BITMAP_H
-
-#include <stdint.h>
-
-extern const uint8_t bw_bitmap[15000];
-extern const uint8_t red_bitmap[15000];
-
-#endif // IMAGE_BITMAP_H
--- zsk/lvgl_1/src/ssd1683_display.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ssd1683.h"
-#include <zephyr/device.h>
-#include <zephyr/drivers/display.h>
-
-int ssd1683_display_init(const struct device *dev);
